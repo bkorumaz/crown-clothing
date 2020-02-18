3c70194957a1f97adf56fbd0ebd89e81711d95ff
Bahadır KORUMAZ <bkorumaz@havelsan.com.tr>
2020-02-18 09:30:41 +0300

    added shoppage with styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shoppage.component';
 import { Route, Switch } from 'react-router-dom'
 
-const HatsPage  = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 
 function App() {
@@ -15,7 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/hats' component={HatsPage}/>
+        <Route  path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
---
+++ src/pages/shoppage/shoppage.component.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import SHOP_DATA from "./shop.data"
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+function ShopPage (props) {
+    const [collections, setCollections] = useState(SHOP_DATA);
+
+return(
+<div className='shop-page'>
+{collections.map( ({id, ...otherCollectionProps}) => ( <CollectionPreview key={id} {...otherCollectionProps} /> ))}
+</div>
+)
+}
+
+export default ShopPage
