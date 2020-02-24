9a62c235589442faf82e19da1130edc99f2c6902
bahadirkorumaz <bahadir.korumaz@gmail.com>
2020-02-24 21:47:39 +0300

    added google auth and currentUser feature but unsubscribe part may need improvement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
 import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/sign-in-sign-up-page/sign-in-sign-up-page.component'
+import { auth } from './firebase/firebase.utils'
 
 import { Route, Switch } from 'react-router-dom'
 
-
 function App() {
+
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+  const unsubscribe = auth.onAuthStateChanged(user => { setCurrentUser(user); console.log(currentUser) })
+  
+  return () => unsubscribe()
+  }, [currentUser]
+  );
+
+
   return (
     <div>
       <Header/>
