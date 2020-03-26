a2b8a6302236b5bce713d755143e7ebc8e78eb6d
bkorumaz <bahadir.korumaz@gmail.com>
2020-03-26 18:21:24 +0300

    added feature for cart-icon to calculate total items in the cart using reduce() function

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setCartVisibility } from '../../redux/cart/cart.actions';
 import './cart-icon.styles.scss';
 
@@ -10,11 +10,13 @@ import './cart-icon.styles.scss';
 function CartIcon () {
 
     const dispatch = useDispatch();
+    const cartItems = useSelector( state => state.cart.cartItems );
 
+    console.log('I am being called');
     return (
     <div className='cart-icon' onClick={() => dispatch(setCartVisibility())}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{ cartItems.reduce((accumulator, {quantity}) => accumulator + quantity, 0) }</span>
     </div>
     );
 };
