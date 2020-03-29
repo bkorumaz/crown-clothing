4dbf5985faac164693f8f908e658823bd81159cb
bkorumaz <bahadir.korumaz@gmail.com>
2020-03-29 23:40:59 +0300

    improved user flow. added toggle cart dropdown visibility to checkout button

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { withRouter } from 'react-router-dom';
+import { setCartVisibility } from '../../redux/cart/cart.actions';
 
 
-const CartDropdown = ({ history }) => {
+const CartDropdown = ({ history, ...otherProps }) => {
     const cartItems = useSelector( state => selectCartItems(state) );
+    const dispatch = useDispatch();
+
+    const handleCheckoutButton = () => {
+        history.push('/checkout');
+        dispatch(setCartVisibility());
+        console.log(otherProps);
+    }
+
     return(
         <div className='cart-dropdown'>
             <div className='cart-items'>
@@ -16,7 +25,7 @@ const CartDropdown = ({ history }) => {
                     cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />) :
                     <span className='empty-message'>Your Cart is Empty</span> }
             </div>
-            <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+            <CustomButton onClick={handleCheckoutButton}>GO TO CHECKOUT</CustomButton>
         </div>
         );
 };
