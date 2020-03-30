04f2acbd84cd0b4053cc4271bfe810aea4c6c411
bkorumaz <bahadir.korumaz@gmail.com>
2020-03-30 03:35:29 +0300

    added feature of removing item from cart into checkout-page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './checkout-item.styles.scss';
+import { useDispatch } from 'react-redux';
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem : { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ( { cartItem } ) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    const dispatch = useDispatch();
 
     return(
         <div className='checkout-item'>
@@ -11,7 +15,7 @@ const CheckoutItem = ({ cartItem : { name, imageUrl, price, quantity } }) => {
             <span className='name'>{name}</span>
             <span className='quantity'>{quantity}</span>
             <span className='price'>{price}</span>
-            <div className='remove-button'>&#10005;</div>
+            <div className='remove-button' onClick={() => dispatch(clearItemFromCart(cartItem))} >&#10005;</div>
         </div>
     );
 }
