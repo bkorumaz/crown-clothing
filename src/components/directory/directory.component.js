06959adb4e24b4d146d524eab2a1445a723a0a63
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-03 05:15:26 +0300

    carried directory data into redux

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,48 +1,11 @@
-import React, {useState} from 'react'
+import React from 'react'
 import "./directory.styles.scss"
 import MenuItem from "../menu-item/menu-item.component"
+import { useSelector}  from 'react-redux'
+import { selectDirectorySections } from '../../redux/directory/directory.selectors'
 
 function Directory (props) {
-    const [sections, setSections] = useState([
-        {
-          title: 'hat',
-          //imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          imageUrl: "/images/hats.png",
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'jackets',
-          //imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          imageUrl: "/images/jackets.png",
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          //imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          imageUrl: "/images/sneakers.png",
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'women',
-          //imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          imageUrl: "/images/women.png",
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/women'
-        },
-        {
-          title: 'men', 
-          //imageUrl: https://i.ibb.co/R70vBrQ/men.png
-          imageUrl: "/images/men.png",
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/men'
-        }
-      ]
-      );
+    const sections = useSelector(state => selectDirectorySections(state));
 
     return (
         <div className="directory-menu">
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; //or we could import sessionStorage
 
@@ -12,7 +13,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers ({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer); //this will return modified version of root reducer expect now with persistence capabilities thanks to this persistReducer function
