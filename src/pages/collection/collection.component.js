aebec9509ed400adb046ea33a28f0f9517332194
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-07 05:10:51 +0300

    completed collection page

--- src/pages/collection/collection.component.js
+++ src/pages/collection/collection.component.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import './collection.styles.scss';
 
 const CollectionPage = ({ match }) => {
+  const collection = useSelector((state) =>
+    selectCollection(match.params.collectionId)(state)
+  ); //investigate this
 
-  const collection = useSelector(state => selectCollection(match.params.collectionId)(state)); //investigate this
+  const { title, items } = collection;
 
-  console.log(collection);
   return (
-    <div className="collection-page">
-      <h2>COLLECTION PAGE</h2>
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        { items.map( item => <CollectionItem key={items.id} item={item}/> )}
+      </div>
     </div>
   );
 };
