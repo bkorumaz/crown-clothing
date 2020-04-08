9be8f27cf3a80641065192d185ce52a55f435187
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-08 05:44:23 +0300

    fixing an error for gh-pages

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -3,37 +3,37 @@ const INITIAL_STATE = {
           [
           {
             title: 'hat',
-            //imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-            imageUrl: "/images/hats.png",
+            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            //imageUrl: "/images/hats.png",
             id: 1,
             linkUrl: 'shop/hats'
           },
           {
             title: 'jackets',
-            //imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-            imageUrl: "/images/jackets.png",
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            //imageUrl: "/images/jackets.png",
             id: 2,
             linkUrl: 'shop/jackets'
           },
           {
             title: 'sneakers',
-            //imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            imageUrl: "/images/sneakers.png",
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            //imageUrl: "/images/sneakers.png",
             id: 3,
             linkUrl: 'shop/sneakers'
           },
           {
             title: 'women',
-            //imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-            imageUrl: "/images/women.png",
+            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+            //imageUrl: "/images/women.png",
             size: 'large',
             id: 4,
             linkUrl: 'shop/womens'
           },
           {
             title: 'men', 
-            //imageUrl: https://i.ibb.co/R70vBrQ/men.png
-            imageUrl: "/images/men.png",
+            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            //imageUrl: "/images/men.png",
             size: 'large',
             id: 5,
             linkUrl: 'shop/mens'
