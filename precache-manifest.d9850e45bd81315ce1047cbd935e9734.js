180352b9c8170b625322f4167e9a8e462d279389
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-08 05:35:40 +0300

    Updates

---
+++ precache-manifest.d9850e45bd81315ce1047cbd935e9734.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2382e6a16b5cd6d9c48a0934b02116",
+    "url": "/e-commerce-web/index.html"
+  },
+  {
+    "revision": "ead053792bce9956594e",
+    "url": "/e-commerce-web/static/css/main.1f2e215a.chunk.css"
+  },
+  {
+    "revision": "9b9b8279858a732a6d79",
+    "url": "/e-commerce-web/static/js/2.45e1268c.chunk.js"
+  },
+  {
+    "revision": "4c86fd10a56eb60a384cd2f2c8426c35",
+    "url": "/e-commerce-web/static/js/2.45e1268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ead053792bce9956594e",
+    "url": "/e-commerce-web/static/js/main.f40645b0.chunk.js"
+  },
+  {
+    "revision": "a43baea45b232d01e10d",
+    "url": "/e-commerce-web/static/js/runtime-main.52bd199b.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/e-commerce-web/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce-web/static/media/shopping-bag.30f07071.svg"
+  }
+]);
