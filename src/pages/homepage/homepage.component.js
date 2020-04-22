1bde1390ff4d58410ee9f1c1109c68a0dbb8724a
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-22 07:37:09 +0300

    added styled components and converted hompageComponent into styled-compnent

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
-
+import { HomePageContainer } from './homepage.styles'; 
 
 function HomePage () {
     return(
-        <div className="homepage">
+        <HomePageContainer>
             <Directory/>
-        </div>
+        </HomePageContainer>
     )
 }
 
