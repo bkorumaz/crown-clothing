e4d4bc131df5e4675b0d8429a394830982398218
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-22 07:40:54 +0300

    reduced code amount in header styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import { auth } from '../../firebase/firebase.utils'
-import {ReactComponent as Logo} from '../../assets/crown.svg'  //special syntax for importing svg in react 
-import './header.styles.scss';
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles';
-import { useSelector } from 'react-redux';
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown.components';
-import { selectCartVisibility } from '../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
+import React from "react";
+import { auth } from "../../firebase/firebase.utils";
+import { ReactComponent as Logo } from "../../assets/crown.svg"; //special syntax for importing svg in react
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+} from "./header.styles";
+import { useSelector } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.components";
+import { selectCartVisibility } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
-function Header () {
+function Header() {
+  const currentUser = useSelector((state) => selectCurrentUser(state));
+  const cartVisibility = useSelector((state) => selectCartVisibility(state));
 
-    const currentUser = useSelector( state => selectCurrentUser(state)  );
-    const cartVisibility = useSelector( state => selectCartVisibility(state) );
+  return (
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo className="logo" />
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="/contact">OPTION</OptionLink>
 
-
-    return(
-        <HeaderContainer>
-            <LogoContainer to='/'>
-                <Logo className='logo' />
-            </LogoContainer>
-            <OptionsContainer>
-                <OptionLink to='/shop'>SHOP</OptionLink>
-                <OptionLink to='/contact'>OPTION</OptionLink>
-
-                {
-                    currentUser ?
-                    <OptionDiv onClick={ () => auth.signOut() }>SIGN-OUT</OptionDiv>
-                    :
-                    <OptionLink to='/signin'>SIGN-IN</OptionLink>
-                }
-                <CartIcon />
-            </OptionsContainer>
-            { cartVisibility ? <CartDropdown/> : null }
-        </HeaderContainer>
-    )
+        {currentUser ? (
+          <OptionLink as="div" onClick={() => auth.signOut()}>
+            SIGN-OUT
+          </OptionLink>
+        ) : (
+          <OptionLink to="/signin">SIGN-IN</OptionLink>
+        )}
+        <CartIcon />
+      </OptionsContainer>
+      {cartVisibility ? <CartDropdown /> : null}
+    </HeaderContainer>
+  );
 }
 
 export default Header;
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,10 +1,5 @@
-import styled, { css } from 'styled-components';
-import { Link } from 'react-router-dom';
-
-const OptionContainerStyles = css`
-padding: 10px 15px;
-cursor: pointer;
-`;
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const HeaderContainer = styled.div`
   height: 70px;
@@ -15,22 +10,20 @@ export const HeaderContainer = styled.div`
 `;
 
 export const LogoContainer = styled(Link)`
-    height: 100%;
-    width: 70px;
-    padding: 25px;
+  height: 100%;
+  width: 70px;
+  padding: 25px;
 `;
 
 export const OptionsContainer = styled.div`
-    width: 50%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 export const OptionLink = styled(Link)`
-${OptionContainerStyles}
-`
-export const OptionDiv = styled.div`
-${OptionContainerStyles}
+  padding: 10px 15px;
+  cursor: pointer;
 `;
