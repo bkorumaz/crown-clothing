9469278e1bf27f26cf0fd72dc4c7330eb20f44dd
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-28 08:29:00 +0300

    getting collections data from backend and updates the frontend accordingly

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import { Route } from 'react-router-dom';
-import CollectionPage from '../collection/collection.component';
+import React, { useEffect } from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import { Route } from "react-router-dom";
+import CollectionPage from "../collection/collection.component";
+import { useDispatch } from "react-redux";
+import {
+  firestore,
+  convertCollectionsSnopshotToMap,
+} from "../../firebase/firebase.utils";
+import { updateCollections } from "../../redux/shop/shop.actions";
 
 function ShopPage({ match }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const collectionsRef = firestore.collection("collections");
+    const unsubscribeFromSnapshot = collectionsRef.onSnapshot(
+      async (snapshot) => {
+        const transformedData = convertCollectionsSnopshotToMap(snapshot)
+        dispatch(updateCollections(transformedData));
+        return unsubscribeFromSnapshot;
+      }
+    );
+  }, []);
+
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
