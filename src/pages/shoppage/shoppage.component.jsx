f360ff7fb3c96df3d5814f10f984c5d70daf3973
bkorumaz <bahadir.korumaz@gmail.com>
2020-05-12 16:43:54 +0300

    getting shop data observable pattern converted to promise pattern

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -19,13 +19,12 @@ function ShopPage({ match }) {
  
   useEffect(() => {
     const collectionsRef = firestore.collection("collections");
-    const unsubscribeFromSnapshot = collectionsRef.onSnapshot((snapshot) => {
+    collectionsRef.get().then((snapshot) => {
       const transformedData = convertCollectionsSnopshotToMap(snapshot);
       dispatch(updateCollections(transformedData));
       setLoading(false);
     });
-    return unsubscribeFromSnapshot; // todo: works in three different ways or does it work?. without unsubscribe or calling this return function in onSnapshot method. or should this be an async func?
-  }, []); //todo: ask this in stackoverflow
+  });
 
   return (
     <div className="shop-page">
