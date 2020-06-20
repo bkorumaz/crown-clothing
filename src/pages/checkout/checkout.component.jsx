9ccc93a2d38932fda0638c05a1bbf39136732d38
bkorumaz <bahadir.korumaz@gmail.com>
2020-06-20 23:11:30 +0300

    CheckoutPage styled components

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,43 +1,54 @@
-import React from 'react';
-import './checkout.styles.scss';
-import { useSelector } from 'react-redux';
-import { selectCartTotalPrice, selectCartItems } from '../../redux/cart/cart.selectors';
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+import React from "react";
+import { useSelector } from "react-redux";
+import {
+  selectCartTotalPrice,
+  selectCartItems,
+} from "../../redux/cart/cart.selectors";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+import {
+  CheckoutPageContainer,
+  CheckoutHeaderContainer,
+  CheckoutHeaderBlockContainer,
+  TotalContainer,
+  WarningContainer,
+} from "./checkout.styles";
 
-function CheckoutPage(){
-    
-    const totalPrice = useSelector( state => selectCartTotalPrice(state) );
-    const cartItems = useSelector( state => selectCartItems(state) );
+function CheckoutPage() {
+  const totalPrice = useSelector((state) => selectCartTotalPrice(state));
+  const cartItems = useSelector((state) => selectCartItems(state));
 
-    return(
-        <div className='checkout-page'>
-            <div className='checkout-header'>
-                <div className='header-item'>
-                    <span>Product</span>
-                </div>
-                <div className='header-item'>
-                    <span>Description</span>
-                </div>
-                <div className='header-item'>
-                    <span>Quantity</span>
-                </div>
-                <div className='header-item'>
-                    <span>Price</span>
-                </div>
-                <div className='header-item'>
-                    <span>Remove</span>
-                </div>
-            </div>
-            {cartItems.map( cartItem => <CheckoutItem key={ cartItem.id } cartItem={ cartItem } /> )}
-            <div className='total'>TOTAL: ${totalPrice}</div>
-            <div className='test-warning'>*Please use the following test credit card for payments*
-            <br/>
-            4242 4242 4242 4242 - Exp: 01/23 - CVV: 123</div>
-            <StripeCheckoutButton price={totalPrice}/>
-        </div>
-    );
-
+  return (
+    <CheckoutPageContainer>
+      <CheckoutHeaderContainer>
+        <CheckoutHeaderBlockContainer>
+          <span>Product</span>
+        </CheckoutHeaderBlockContainer>
+        <CheckoutHeaderBlockContainer>
+          <span>Description</span>
+        </CheckoutHeaderBlockContainer>
+        <CheckoutHeaderBlockContainer>
+          <span>Quantity</span>
+        </CheckoutHeaderBlockContainer>
+        <CheckoutHeaderBlockContainer>
+          <span>Price</span>
+        </CheckoutHeaderBlockContainer>
+        <CheckoutHeaderBlockContainer>
+          <span>Remove</span>
+        </CheckoutHeaderBlockContainer>
+      </CheckoutHeaderContainer>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <TotalContainer>TOTAL: ${totalPrice}</TotalContainer>
+      <WarningContainer>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
+      </WarningContainer>
+      <StripeCheckoutButton price={totalPrice} />
+    </CheckoutPageContainer>
+  );
 }
 
 export default CheckoutPage;
--- src/pages/checkout/checkout.styles.jsx
+++ src/pages/checkout/checkout.styles.jsx
@@ -1,48 +1,37 @@
 import styled from "styled-components";
 
 export const CheckoutPageContainer = styled.div`
-width: 55%;
-    min-height: 90vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 50px auto 0;
-`
-export const CheckoutHeader = styled(span)`
-width: 100%;
-padding: 10px 0;
-display: flex;
-justify-content: space-between;
-border-bottom: 1px solid darkgrey;
-`
-export const CheckoutHeaderBlock = styled.div`
-text-transform: capitalize;
-        width: 23%;
-  
-        &:last-child {
-          width: 8%;
-        }
-`
-  
-    .checkout-header {
-  
-      .header-block {
-        
-      }
-    }
-  
-    .total {
-      margin-top: 30px;
-      margin-left: auto;
-      font-size: 36px;
-    }
+  width: 55%;
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px auto 0;
+`;
+export const CheckoutHeaderContainer = styled.div`
+  width: 100%;
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid darkgrey;
+`;
+export const CheckoutHeaderBlockContainer = styled.div`
+  text-transform: capitalize;
+  width: 23%;
 
-    .test-warning {
-      text-align: center;
-      margin-top: 40px;
-      margin-bottom:20px;
-      font-size: 24px;
-      color: red;
-    }
+  &:last-child {
+    width: 8%;
   }
-  
+`;
+export const TotalContainer = styled.div`
+  margin-top: 30px;
+  margin-left: auto;
+  font-size: 36px;
+`;
+export const WarningContainer = styled.div`
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 20px;
+  font-size: 24px;
+  color: red;
+`;
