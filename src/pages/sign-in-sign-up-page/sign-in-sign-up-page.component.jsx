083378ad36bed7b95232eaa9919c998a2cd47395
bkorumaz <bahadir.korumaz@gmail.com>
2020-06-20 21:47:21 +0300

    signInSignUpPageContainer styled component

--- src/pages/sign-in-sign-up-page/sign-in-sign-up-page.component.jsx
+++ src/pages/sign-in-sign-up-page/sign-in-sign-up-page.component.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 
-import './sign-in-sign-up-page.styles.scss';
+import { SignInSignUpPageContainer } from './sign-in-sign-up-page.styles';
 
 
 function SignInSignUpPage() {
     return(
-        <div className='sign-in-and-sign-up'>
+        <SignInSignUpPageContainer>
             <SignIn/>
             <SignUp/>
-        </div>
+        </SignInSignUpPageContainer>
     )
 }
 
